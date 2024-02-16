09d879af87bd2250cd628887b9f778f3957052c2
Bear Revels <bear.revels@gmail.com>
2024-02-16 08:54:55 +0100

    update

---
+++ src/scraper.py
@@ -0,0 +1,21 @@
+import requests
+
+#WikipediaScraper object that allows structured data retrieval from designated APIs
+class WikipediaScraper:
+    def __init__(self):
+        self.base_url: str = https://country-leaders.onrender.com
+        self.country_endpoint: str = https://country-leaders.onrender.com/countries
+        self.leaders_endpoint: str = https://country-leaders.onrender.com/leaders
+        self.cookies_endpiont: str = https://country-leaders.onrender.com/cookie
+        self.leaders_data: dict = {}
+        self.cookie: object = 
+
+    def refresh_cookie(self):
+
+    def get_countries(self):
+    
+    def get_leaders(self):
+
+    def get_first_paragraph(self):
+    
+    def to_json_file(self):
